29d84e7c57150a8cae449340c9d34a336e826545
Adrien Davy <adriendavyfx@gmail.com>
2023-03-19 20:57:43 +0100

    Parallax effect on clouds Done

--- src/pages/welcome/Welcome.jsx
+++ src/pages/welcome/Welcome.jsx
@@ -1,12 +1,50 @@
-import React from 'react';
-import './Welcome.css';
+import React, { useState } from "react";
+import "./Welcome.css";
+//PICTURES
+import BigCloud from "../../assets/bigcloud.png";
+import Cloud1 from "../../assets/cloud1.png";
+import Cloud2 from "../../assets/cloud2.png";
 
 const Welcome = () => {
-  return (
-    <div>
-      Welcome
-    </div>
-  )
-}
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
 
-export default Welcome
+  const handleMouseMove = (e) => {
+    setMousePosition({
+      x: e.pageX,
+      y: e.pageY,
+    });
+  };
+
+  return (
+    <div className="sky" onMouseMove={handleMouseMove}>
+      <div className="clouds-container">
+        <img
+          src={BigCloud}
+          alt="cloud"
+          className="bigcloud"
+          style={{
+            transform: `translate(calc(-50% + ${mousePosition.x / 50}px), calc(-50% + ${mousePosition.y / 50}px))`,
+          }}
+        />
+        <img
+          src={Cloud1}
+          alt="cloud"
+          className="cloud1"
+          style={{
+            transform: `translate(calc(-50% + ${mousePosition.x / 70}px), calc(-50% + ${mousePosition.y / 70}px))`,
+          }}
+        />
+        <img
+          src={Cloud2}
+          alt="cloud"
+          className="cloud2"
+          style={{
+            transform: `translate(calc(-50% + ${mousePosition.x / 100}px), calc(-50% + ${mousePosition.y / 100}px))`,
+          }}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Welcome;
