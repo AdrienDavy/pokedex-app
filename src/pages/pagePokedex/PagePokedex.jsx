b5fe481761f7cb4a347a39744f852cb766f05544
Adrien Davy <adriendavyfx@gmail.com>
2023-03-25 13:54:55 +0100

    import podex svg

--- src/pages/pagePokedex/PagePokedex.jsx
+++ src/pages/pagePokedex/PagePokedex.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
-
-import './PagePokedex.css'
+import './PagePokedex.css';
+import Pokedex from '../../components/pokedex/Pokedex';
+import { useTransition } from '../../contexts/TransitionContext';
 
 const PagePokedex = () => {
+  const { isHidden, setIsHidden } = useTransition();
   return (
-    
-    <div>
-      PagePokedex
+
+    <div className={isHidden ? 'visible' : 'hidden'}>
+      <Pokedex />
     </div>
-   
+
   )
 }
 
