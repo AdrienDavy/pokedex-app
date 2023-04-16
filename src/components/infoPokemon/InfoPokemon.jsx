c11b6b7e3f28336df419a110bc6b8e48b6ee6869
Adrien Davy <adriendavyfx@gmail.com>
2023-04-16 02:23:06 +0200

    images types setup

--- src/components/infoPokemon/InfoPokemon.jsx
+++ src/components/infoPokemon/InfoPokemon.jsx
@@ -15,8 +15,10 @@ const InfoPokemon = () => {
         <li>Type:{pokemonType} </li>
         <li>Taille: {pokemonInfos?.height}</li>
         <li>Poids: {pokemonInfos?.weight}</li>
-        {pokemonTypeImage[0] ? <img src={pokemonTypeImage[0]} alt={pokemonData.name} className='type-image' /> : null}
-        {pokemonTypeImage[1] ? <img src={pokemonTypeImage[1]} alt={pokemonData.name} className='type-second-image' /> : null}
+        <div className='image-container'>
+          {pokemonTypeImage[0] ? <img src={pokemonTypeImage[0]} alt={pokemonData.name} className='type-image' /> : null}
+          {pokemonTypeImage[1] ? <img src={pokemonTypeImage[1]} alt={pokemonData.name} className='type-second-image' /> : null}
+        </div>
       </ul>
 
       <img src={pokedexRight} alt="pokedex" className='pokedex-right' />
